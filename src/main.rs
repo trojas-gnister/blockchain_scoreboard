808727a56aa8e8a75e32293d91a7ed6b7f4bd16a
Troy <iskrydev@gmail.com>
2024-09-17 21:13:03 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,13 @@
 
 use actix_web::{web, App, HttpServer, Responder, HttpResponse};
-use std::sync::Mutex;
 mod blockchain;
-use serde::Deserialize
 
-#[derive(Serialize, Deserialize)]
-struct Score {
-    username: String,
-    score: u32
-}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(index)
-            .service(submit_score)
-            .service(get_scores)
     })
     .bind("localhost:8000")?
     .run()
