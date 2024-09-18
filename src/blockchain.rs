3f0f82ea8280f4e438fa0b64a51a591010a99a4a
trojas-gnister <iskrydev@gmail.com>
2024-09-17 20:49:28 -0500

    updated blockchain file. created user struct

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -3,23 +3,35 @@ use serde::{Deserialize, Serialize};
 use sha2::{Digest, Sha256};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
+
+// frontend should take a players name. TODO: frontend needs to track score and playuer name
+pub struct User {
+    pub player_name: String,
+    pub score: u32,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Block {
     //Position of the block in the blockchain
     pub index: u64,
     // Time when the block was created
     pub timestamp: DateTime<Utc>,
-    // Data stored in the block NOTE:  score and maybe IP? or user accounts
-    pub data: String,
-    // hash of previous block NOTE: not sure if we need this?
+    // Data stored in the block
+    pub data: User,
+    // hash of previous block
     pub previous_hash: String,
     //NOTE: hash of current block
     pub hash: String,
 }
 
 impl Block {
-    pub fn new(index: u64, data: String, previous_hash: String) -> Self {
+    pub fn new(index: u64, data: User, previous_hash: String) -> Self {
         let timestamp = Utc::now();
-        let hash = Block::calculate_hash(index, &timestamp, &data, &previous_hash);
+        let user_data = User {
+            player_name: "test".to_string(),
+            score: 33,
+        };
+        let hash = Block::calculate_hash(index, &timestamp, user_data, &previous_hash);
         Block {
             index,
             timestamp,
@@ -32,13 +44,18 @@ impl Block {
     pub fn calculate_hash(
         index: u64,
         timestamp: &DateTime<Utc>,
-        data: &str,
+        data: User,
         previous_hash: &str,
     ) -> String {
-        let input = format!("{}{}{}{}", index, timestamp, data, previous_hash);
+        // user block
+        let input = format!("{}{}{:?}{}", index, timestamp, data, previous_hash);
+        // will be used to hash inputs. will hash using Sha256
         let mut hasher = Sha256::new();
+        // use hasher to hash the users block (input)
         hasher.update(input.as_bytes());
+        // hash result
         let result = hasher.finalize();
+        // converts hash to hexadecimal string
         hex::encode(result)
     }
 }
